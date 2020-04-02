14c5710d3a6413fb519c5698de8a7d8ca07c7156
xuchg1 <xuchg1@yonyou.com>
2020-04-02 21:35:38 +0800

    Toast

--- src/components/UIComponents/Toast.ts
+++ src/components/UIComponents/Toast.ts
@@ -19,11 +19,13 @@ export class Toast{
     item: CanvasItem;
     animateItem: AnimateItem | undefined;
     closeAnimateItem: AnimateItem | undefined;
-    constructor(control: Control, createCanvasFunc: any, background: string, radius: number, time: number){
+    sliceTime: number;
+    constructor(control: Control, createCanvasFunc: any, background: string, radius: number, time: number, sliceTime: number){
         this.control = control;
         this.background = background;
         this.radius = radius;
         this.time = time;
+        this.sliceTime = sliceTime;
         this.canvas = createCanvasFunc();
         this.canvas.width = window.innerWidth * window.devicePixelRatio;
         this.canvas.height = window.innerHeight * window.devicePixelRatio;
@@ -32,40 +34,76 @@ export class Toast{
             this.canCtx.scale(window.devicePixelRatio, window.devicePixelRatio);
         }
         this.item = new CanvasItem(new Vector3(0,0,0), window.innerWidth, window.innerHeight, this.canvas);
-        this.item.setTouchEvent(this.clear.bind(this));
-    }
-    showLines(textArr: TextConfig[], width=window.innerWidth * 0.6, height= window.innerHeight * 0.2): void{
+        this.item.setTouchEvent(this.close.bind(this));
 
+        if(this.item.mesh !== undefined){
+            this.animateItem = new AnimateItem(this.item.mesh, AnimateType.ONCE, AnimateMode.SHAPE, this.sliceTime);
+            this.closeAnimateItem = new AnimateItem(this.item.mesh, AnimateType.ONCE, AnimateMode.SHAPE, this.sliceTime);
+        }
     }
-    show(text: string, width=window.innerWidth * 0.6, height= window.innerHeight * 0.2): void{
+    showLines(textArr: TextConfig[], width=window.innerWidth * 0.6, height= window.innerHeight * 0.2, top = window.innerHeight * 0.2, paddingTop = window.innerHeight * 0.1): void{
         this.clear();
         if(this.canCtx !== null && this.item.mesh !== undefined){
             this.canCtx.clearRect(0, 0, window.innerWidth, window.innerHeight);
             let left = ( window.innerWidth - width ) / 2;
-            let top = window.innerHeight * 0.2;
             this.fillRoundRect(this.canCtx, left, top, width, height, this.radius, this.background);
-            this.canCtx.font = 10 * window.devicePixelRatio + 'px " 宋体';
-            this.canCtx.fillStyle = '#ffffff';
             this.canCtx.textAlign = 'center';
-            this.canCtx.fillText(text, window.innerWidth * 0.5, window.innerHeight * 0.3); 
-            this.item.mesh.scale.set(0,0,0);
-            this.item.update();
-            this.control.itemManager.addHudItem(this.item);
-            this.animateItem = new AnimateItem(this.item.mesh, AnimateType.ONCE, AnimateMode.SHAPE, 1);
-            this.animateItem.setTargetPosition(new Vector3(1,1,1), 0, Power4.easeIn);
-            this.animateItem.action();
-
-            this.closeAnimateItem = new AnimateItem(this.item.mesh, AnimateType.ONCE, AnimateMode.SHAPE, 1);
-            this.closeAnimateItem.setTargetPosition(new Vector3(0,0,0), this.time + 1, Power4.easeIn);
+            let fontTotalHeight = 0;
+            for(let i=0;i<textArr.length;i++){
+                let text = textArr[i];
+                let deviceFontSize = text.fontSize * window.devicePixelRatio;
+                this.canCtx.font = deviceFontSize + 'px " 宋体';
+                this.canCtx.fillStyle = text.color;
+                this.canCtx.fillText(text.text, window.innerWidth * 0.5, top + paddingTop + fontTotalHeight); 
+                fontTotalHeight = fontTotalHeight + deviceFontSize + 5;
+            }
+            this.animate();
+        }
+    }
+    show(text: string, fontSize: 10, color = '#ffffff', width = window.innerWidth * 0.6, height = window.innerHeight * 0.2, top = window.innerHeight * 0.2, paddingTop = window.innerHeight * 0.1): void{
+        this.clear();
+        if(this.canCtx !== null){
+            this.canCtx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+            let left = ( window.innerWidth - width ) / 2;
+            this.fillRoundRect(this.canCtx, left, top, width, height, this.radius, this.background);
+            this.canCtx.font = fontSize * window.devicePixelRatio + 'px " 宋体';
+            this.canCtx.fillStyle = color;
+            this.canCtx.textAlign = 'center';
+            this.canCtx.fillText(text, window.innerWidth * 0.5, top + paddingTop); 
+            this.animate();
+        }
+    }
+    close(): void{
+        if(this.closeAnimateItem !== undefined){
+            this.closeAnimateItem.pause();
+            this.closeAnimateItem.setTargetPosition(new Vector3(0,1,1), 0, Power4.easeIn);
             this.closeAnimateItem.action(this.clear.bind(this));
         }
     }
-    clear(): void{
+    private animate(): void{
+        if(this.item.mesh !== undefined){
+            this.item.mesh.scale.set(0,1,1);
+            this.item.update();
+            this.control.itemManager.addHudItem(this.item);
+    
+            if(this.animateItem !== undefined && this.closeAnimateItem !== undefined){
+                this.animateItem.setTargetPosition(new Vector3(1,1,1), 0, Power4.easeIn);
+                this.animateItem.action();
+                
+                this.closeAnimateItem.setTargetPosition(new Vector3(0,1,1), this.time + this.sliceTime, Power4.easeIn);
+                this.closeAnimateItem.action(this.clear.bind(this));
+            }
+        }
+    }
+    private clear(): void{
         if(this.item.uuid !== undefined){
+            if(this.closeAnimateItem !== undefined){
+                this.closeAnimateItem.pause();
+            }
             this.control.itemManager.remove(this.item.uuid);
         }
     }
-    fillRoundRect(cxt: CanvasRenderingContext2D, x: number, y: number, width: number, height: number, radius: number, fillColor: string) {
+    private fillRoundRect(cxt: CanvasRenderingContext2D, x: number, y: number, width: number, height: number, radius: number, fillColor: string) {
         if (2 * radius > width || 2 * radius > height) { return false; }
         cxt.save();
         cxt.translate(x, y);
@@ -74,7 +112,7 @@ export class Toast{
         cxt.fill();
         cxt.restore();
     }
-    drawRoundRectPath(cxt: CanvasRenderingContext2D, width: number, height: number, radius: number) {
+    private drawRoundRectPath(cxt: CanvasRenderingContext2D, width: number, height: number, radius: number) {
         cxt.beginPath();
         cxt.arc(width - radius, height - radius, radius, 0, Math.PI / 2);
         cxt.lineTo(radius, height);
@@ -88,7 +126,7 @@ export class Toast{
     }
 }
 export class TextToast extends Toast{
-    constructor(control: Control, createCanvasFunc: any, background='rgba(0,0,0,0.7)', radius=10, time=3){
-        super(control, createCanvasFunc, background, radius, time);
+    constructor(control: Control, createCanvasFunc: any, background='rgba(0,0,0,0.7)', radius=10, time=3, sliceTime=0.5){
+        super(control, createCanvasFunc, background, radius, time, sliceTime);
     }
 }
