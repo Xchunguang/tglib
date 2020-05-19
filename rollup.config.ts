fd2ecaec3d31ebaf6608c40e8ba79cdd7dd53243
xuchg1 <xuchg1@yonyou.com>
2020-05-19 14:28:13 +0800

    update

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,7 +7,7 @@ import json from 'rollup-plugin-json'
 
 const pkg = require('./package.json')
 
-const libraryName = 'tglib'
+const libraryName = 'threeglib'
 
 export default {
   input: `src/${libraryName}.ts`,
